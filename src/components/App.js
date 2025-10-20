f669b1052d3b55abefd9d80a20308d7c794394e6
Collins Kibet <kollcibe05@gmail.com>
2025-10-20 23:46:39 +0300

    IssueWithTest

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+function App () {
+const [image, setImage] = useState(null)
+const [isLoading, setLoading]  = useState(true)
+
+useEffect(() => {
+    fetch ("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json())
+    .then(data => {
+        setImage(data.message)
+        setLoading(false)
+    })
+    
+}, [])
+
+return (
+    <div>
+    <p>{isLoading? "Loading..." : null}</p>    
+    <img src={image} alt="A Random Dog"/>
+    </div>
+)
+
+}
+
+
+export default App;
+
+
+
